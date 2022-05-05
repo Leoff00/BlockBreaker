554c3f659330a3360930704a1836b43d09988207
leoff00 <leof0180@gmail.com>
2022-05-04 23:45:42 -0300

    adding blocks dinamically

--- src/script.js
+++ src/script.js
@@ -1,7 +1,31 @@
 class BlockBreaker {
+  BLOCKS_LENGTH = 20;
+
   constructor() {
+    this.createBlocks();
+  }
+
+  createBlocks() {
+    const blockContainer = document.querySelector("#blocksContainer");
+
+    for (let i = 0; i < this.BLOCKS_LENGTH; i++) {
+      const blocks = document.createElement("div");
+      blocks.className = "blocks";
+      blocks.style.display = "block";
+      blockContainer.appendChild(blocks);
+    }
+  }
+
+  ballAction() {
     //TODO: IMPLEMENTATION
-    console.log("running...");
+  }
+
+  platformAction() {
+    //TODO: IMPLEMENTATION
+  }
+
+  breakBlocks() {
+    //TODO: IMPLEMENTATION
   }
 }
 
